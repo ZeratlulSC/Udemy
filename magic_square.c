823715964d7d1e560e33039d4d04ebf39c041421
Nikolai <n.klokov@mss.su>
2021-10-18 14:56:12 +0300

    Cppcheck

--- magic_square.c
+++ magic_square.c
@@ -4,6 +4,26 @@
 
 int main(void)
 {
-    
-    return 0;
+    int dim = 0;
+    printf("Enter dimension of Magic Square (Only ODD dimension >=1 and <=15 accepted): ");
+    scanf("%d", &dim);
+    if (dim%2==0 || dim < 1 || dim > 15){
+        printf("Invalid input\nValid values are odd integers between 1 and 15");
+        return 0;
+    }
+    int arr [dim][dim];
+    for (int i = 0; i < dim; i++){
+        for (int j = 0; j < dim; j++) arr[i][j] = 0;
+    }
+    int row = dim - 1;
+    int column = dim/2;
+    for(int k = 1; k < dim*dim; k++){
+        arr[row++][column++] = k;
+        if (row == dim) row = 0;
+        if (column == dim) dim = 0;
+        if (arr[row][column] != 0){
+        
+        }
+    }
+    return 0;
 }
