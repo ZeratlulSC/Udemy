9422f5eef4bedac10574292298a300ec1589104d
Nikolai <n.klokov@mss.su>
2021-10-25 15:36:01 +0300

    upd 1

--- files/file_1.c
+++ files/file_1.c
@@ -2,6 +2,7 @@
 
 int main (){
     FILE *fp;
+    
     fp = fopen("1.txt", "r");
     if (fp == NULL) {
         printf ("Unable to open");
@@ -12,5 +13,11 @@ int main (){
         printf("%c", ch);
         ch = fgetc(fp);
     }
+    printf("\n");
+    fclose(fp);
+
+    char str[30];
+    fp = fopen("1.txt", "r");
+    printf("%s", fgets(str, 30, fp));
     fclose(fp);
 }
