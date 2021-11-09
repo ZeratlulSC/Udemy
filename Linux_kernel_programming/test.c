dca33f05c4a8597ffecbf02cf17d466c6642f644
Nick <nikolai.klokov@gmail.com>
2021-11-09 16:11:53 +0300

    working dir update for connection with home pc

--- Linux_kernel_programming/test.c
+++ Linux_kernel_programming/test.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 
 int main (int argc, char **argv){
-    int nmbr_of_lines = 1;
     FILE *pfile = NULL;
-    char *filename = "../.gitignore";
+    FILE *newfile = NULL;
+    char *filename = "readme";
+    char *filename1 = "new_file.txt";
     pfile = fopen(filename, "r");
-    if (pfile == NULL) return -1;
+    newfile = fopen(filename1, "w");
+    if (pfile == NULL || newfile == NULL) return -1;
     char ch = fgetc(pfile);
-    while (ch!= EOF)
+    while (ch != EOF)
     {
+        fputc(tolower(ch), newfile);
         ch = fgetc(pfile);
-        if (ch == '\n') nmbr_of_lines++;
     }
+    rename(filename1, filename);
     fclose(pfile);
+    fclose(newfile);
+    remove(filename1);
     pfile = NULL;
-    printf("%d\n", nmbr_of_lines);
+    newfile = NULL;
     return 0;
 }
