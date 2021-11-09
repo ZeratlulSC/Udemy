11809211c9c63eb16838cc8075c4d0bcd807cfd3
Nick <nikolai.klokov@gmail.com>
2021-11-09 15:57:23 +0300

    upd with new task

--- Linux_kernel_programming/test.c
+++ Linux_kernel_programming/test.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
-struct employee 
-{
-    int hireDate;
-    float salary;
-    char name[40];
-};
 
 int main (int argc, char **argv){
-    struct employee emp1;
-    emp1.hireDate = 8112021;
-    strcpy(emp1.name, "Jane");
-    emp1.salary = 524.2;
-    struct employee emp2;
-    scanf("%d %f %s", &(emp2.hireDate), &(emp2.salary), emp2.name);
-    printf("%s %f %d\n%s %f %d\n", emp1.name, emp1.salary, emp1.hireDate, emp2.name, emp2.salary, emp2.hireDate);
+    int nmbr_of_lines = 1;
+    FILE *pfile = NULL;
+    char *filename = "../.gitignore";
+    pfile = fopen(filename, "r");
+    if (pfile == NULL) return -1;
+    char ch = fgetc(pfile);
+    while (ch!= EOF)
+    {
+        ch = fgetc(pfile);
+        if (ch == '\n') nmbr_of_lines++;
+    }
+    fclose(pfile);
+    pfile = NULL;
+    printf("%d\n", nmbr_of_lines);
     return 0;
 }
